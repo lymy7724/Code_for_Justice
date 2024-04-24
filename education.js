522fd1d149a5d3aee25d6a01ca9bd3b7f14dbfb6
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-04-24 16:43:51 -0400

    ricardo push

--- education.js
+++ education.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.toggle-button');
 const links = document.querySelector('.navbar-links');
-let timeout;
+
 toggleBtn.addEventListener('click',function(){
 
     links.classList.toggle('active');
